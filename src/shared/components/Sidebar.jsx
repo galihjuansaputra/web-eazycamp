3b87ba9f8ca2cbbed6deb9294567564559ed9a6a
Galih Prastya Juansaputra <galih.juansaputra@gmail.com>
2024-05-06 10:14:18 +0700

    [FEATURE] add logout

--- src/shared/components/Sidebar.jsx
+++ src/shared/components/Sidebar.jsx
@@ -1,7 +1,25 @@
 import {IconBuildingStore, IconDashboard, IconLogout, IconReceipt, IconUserCog, IconUsers} from "@tabler/icons-react";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import Swal from "sweetalert2";
 
 function Sidebar() {
+    const navigate = useNavigate();
+    const handleLogout = () => {
+
+        Swal.fire({
+            title: "Do you want to log out?",
+            showCancelButton: true,
+            confirmButtonText: "Log Out",
+            confirmButtonColor: "red"
+        }).then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+                localStorage.clear();
+                navigate('/login')
+            }
+        });
+    }
+
     return (
         <>
             <div className="border-end bg-dark" id="sidebar-wrapper">
@@ -39,10 +57,10 @@ function Sidebar() {
                             Admin
                     </Link>
 
-                    <Link to="/login" className="text-decoration-none d-flex align-items-center cursor-pointer border-0 list-group-item list-group-item-action list-group-item-dark p-3 mb-2 rounded-4">
+                    <a onClick={handleLogout} className="text-decoration-none d-flex align-items-center cursor-pointer border-0 list-group-item list-group-item-action list-group-item-dark p-3 mb-2 rounded-4">
                             <IconLogout style={{marginRight: 10}}/>
                             Log Out
-                    </Link>
+                    </a>
                 </div>
             </div>
         </>
